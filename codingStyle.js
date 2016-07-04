412741bda03a66b8a605bf8cd1bb3797a479c99a
JessMitch <jessicamitch42@gmail.com>
2016-07-04 20:43:48 +0000

    added section on functions

--- codingStyle.js
+++ codingStyle.js
@@ -5,8 +5,30 @@ var name = "Jess";
 
 var num = 10;
 
-if (num > 0) {
+if (num > 0) { // always have curly brackets for if/else/for/while
+// don't techinically need curly's for one command but it's proper syntax
     console.log("The number is greater than zero.");
-};
-    console.log("good job!");
+}
+console.log("good job!");
+
+// functions
+function doSomething(arg1, arg2) {
+    // code here;
+}
+
+var doSomething = function(arg1, arg2) {
+    // code here;
+}
+
+var doSomething = function doSomething(arg1, arg2) {
+    // code here;
+}
+
+/* multiple line comments
+functions above are not all equal.
+Use first version
+*/
+
+
+
     
